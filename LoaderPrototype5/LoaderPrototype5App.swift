18032cd1b02c674a2227d43206a1c8c2971eb3f0
Luka Lešić <lukalesic20@gmail.com>
2022-09-15 02:07:36 +0200

    Initial Commit

---
+++ LoaderPrototype5/LoaderPrototype5App.swift
@@ -0,0 +1,17 @@
+//
+//  LoaderPrototype5App.swift
+//  LoaderPrototype5
+//
+//  Created by Luka Lešić on 15.09.2022..
+//
+
+import SwiftUI
+
+@main
+struct LoaderPrototype5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
