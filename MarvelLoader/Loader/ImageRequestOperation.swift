07dc6e064733e3376b64786f16af45e03ece2304
Luka Lešić <lukalesic20@gmail.com>
2022-10-11 10:50:04 +0200

    updates return from cache functionality

--- MarvelLoader/Loader/ImageRequestOperation.swift
+++ MarvelLoader/Loader/ImageRequestOperation.swift
@@ -35,61 +35,58 @@ class ImageRequestOperation: AsynchronousOperation {
         let fileKey = ImageRequestOperation.key(from: request)
         let decode = JSONDecoder()
         
-//        if let image = cache.getImageWithKey(fileKey) {
-  //          DispatchQueue.main.async {
-    //            print("Displaying image from cache")
+        DispatchQueue.main.async {
+            
+            self.task = self.session.dataTask(with: self.request) {data, response, error in
                 
-      //       self.completionHandler(.success(image))
-        //       self.finish()
-          //  }
-      //  }
-    // else {
-            DispatchQueue.main.async {
+                guard
+                    let response = response as? HTTPURLResponse,
+                    200 ..< 300 ~= response.statusCode
+                else {
+                    self.completionHandler(.failure(error ?? URLError(.badServerResponse)))
+                    self.finish()
+                    return
+                }
                 
-                self.task = self.session.dataTask(with: self.request) {data, response, error in
-                    
-                            guard
-                                let response = response as? HTTPURLResponse,
-                                200 ..< 300 ~= response.statusCode
-                            else {
-                                self.completionHandler(.failure(error ?? URLError(.badServerResponse)))
-                                self.finish()
-                                return
-                            }
+                if let data = data{
+                    do {
+                        
+                        if let image = UIImage(data: data) {
                             
-                            if let data = data{
-                                do {
-                                    //code to download image from Url and then cache, UIImage(data:data) does not return image - append file and ext
-                              //  let image = try UIImage(data: data) ?? UIImage(systemName: "scribble")!
-
-                                    if let image = UIImage(data: data) {
-                                        self.cache.saveImageToCache(image, key: fileKey)
-                                        DispatchQueue.main.async { self.completionHandler(.success(image)) }
-                                    }
-                           
-                                    else{
-                                        let result = try decode.decode(ComicBook.self, from: data)
-                                            DispatchQueue.main.async {
-                                                print("data loaded!!")
-                                                print(result)
-                                                self.completionHandler(.success(result)) // send back the object on completion
-
-                                            }
-                                    }
-                                    
-                             
-                                    self.finish()
-                                } catch let error{
-                                    self.completionHandler(.failure(error))
+                            if let image = self.cache.getImageWithKey(fileKey) {
+                                DispatchQueue.main.async {
+                                    print("Displaying image from cache")
+                                    self.completionHandler(.success(image))
                                 }
                             }
+                            
+                            else{
+                                self.cache.saveImageToCache(image, key: fileKey)
+                                DispatchQueue.main.async { self.completionHandler(.success(image)) }
+                                
+                            }
                         }
-                self.task.resume()
-                    return
-                
-                self.finish()
+                        
+                        else{
+                            let result = try decode.decode(ComicBook.self, from: data)
+                            DispatchQueue.main.async {
+                                print("data loaded!!")
+                                print(result)
+                                self.completionHandler(.success(result)) // send back the object on completion
+                                
+                            }
+                        }
+                        self.finish()
+                    } catch let error{
+                        self.completionHandler(.failure(error))
+                    }
+                }
             }
-      //  }
+            self.task.resume()
+            return
+            
+            self.finish()
+        }
     }
     
     override func cancel() {
